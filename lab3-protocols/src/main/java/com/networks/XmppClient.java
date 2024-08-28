21ba8987aa9e50e533577635e4d1b68d5a623214
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2024-08-28 12:04:58 -0600

    :tada: init repo , missing classes for FloodingNodes, Link State Routing Nodes for the algorithms

---
+++ lab3-protocols/src/main/java/com/networks/XmppClient.java
@@ -0,0 +1,45 @@
+package com.networks;
+
+import java.io.IOException;
+
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.tcp.XMPPTCPConnection;
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
+/**
+ * Class for handling XMPP connection and messaging process
+ */
+public class XmppClient {
+    private XMPPTCPConnection connection = null;
+
+    /**
+     * 
+     * @param JID the JID composed of username@domain
+     * @param password the pwword
+     * @throws SmackException
+     * @throws IOException
+     * @throws XMPPException 
+     * @throws InterruptedException
+     */
+    public XmppClient (String JID, String password) throws SmackException, IOException, XMPPException, InterruptedException { 
+        String domain = JID.split(":")[1];
+        XMPPTCPConnectionConfiguration config = XMPPTCPConnectionConfiguration.builder()
+            .setUsernameAndPassword(JID, password)
+            .setXmppDomain(domain)
+            .setHost(domain)
+            .setPort(5222)
+            .setSecurityMode(ConnectionConfiguration.SecurityMode.disabled)
+            .build();
+
+            this.connection = new XMPPTCPConnection(config);
+            this.connection.connect();
+            this.connection.login();
+    }
+
+    public void disconnect() {
+        if (this.connection != null && connection.isConnected()) {
+            this.connection.disconnect();
+        }
+    }
+}
